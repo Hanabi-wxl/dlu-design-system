a01bcba31374269fabab713cabab36e0592a2429
567jiong <11586903+jiong567@user.noreply.gitee.com>
2023-05-09 22:23:51 +0800

    feat: 新增了增删改查学校功能 feat: 新建了harder/info下面的请求绑定类

---
+++ handler/info/requestModel.go
@@ -0,0 +1,11 @@
+package handler
+
+// @Description: 定义绑定数据类型
+type PageRequest struct {
+	Size int
+	Num  int
+}
+
+type IdRequest struct {
+	Id string
+}
--- handler/info/school.go
+++ handler/info/school.go
@@ -1,13 +1,18 @@
 package handler
 
 import (
-	"net/http"
-
+	"github.com/Hanabi-wxl/dlu-design-system/dal/model"
 	"github.com/Hanabi-wxl/dlu-design-system/pkg/result"
 	service "github.com/Hanabi-wxl/dlu-design-system/service/info"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
+// GetSchoolList
+//
+//	@Description: 分页查询学校 参数: PageRequest
+//	@param c
 func GetSchoolList(c *gin.Context) {
 	var page PageRequest
 	if err := c.ShouldBindUri(&page); err != nil {
@@ -23,18 +28,91 @@ func GetSchoolList(c *gin.Context) {
 	}
 }
 
+// AddSchool
+//
+//	@Description: 新增学校信息 参数: school (JSON)
+//	@param c
 func AddSchool(c *gin.Context) {
+	var school model.School
+	if err := c.ShouldBindJSON(&school); err != nil {
+		c.JSON(http.StatusBadRequest, result.NewFailedResult(err.Error()))
+		return
+	}
+	infoService := service.GetInfoService()
+	err := infoService().AddSchool(&school)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+	} else {
+		c.JSON(http.StatusOK, result.Ok())
+	}
 
 }
 
+// DeleteSchool
+//
+//	@Description: 删除学校信息 参数:IdRequest
+//	@param c
 func DeleteSchool(c *gin.Context) {
+	var idRequest IdRequest
+	if err := c.ShouldBindUri(&idRequest); err != nil {
+		c.JSON(http.StatusBadRequest, result.NewFailedResult(err.Error()))
+		return
+	}
+	id, err := strconv.ParseInt(idRequest.Id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, result.NewFailedResult(err.Error()))
+		return
+	}
+	infoService := service.GetInfoService()
+	err2 := infoService().DeleteSchool(id)
+	if err2 != nil {
+		c.JSON(http.StatusBadRequest, err2)
+	} else {
+		c.JSON(http.StatusOK, result.Ok())
+	}
 
 }
 
+// UpdateSchool
+//
+//	@Description: 更新学校信息 参数: school (JSON)
+//	@param c
 func UpdateSchool(c *gin.Context) {
+	var school model.School
+	if err := c.ShouldBindJSON(&school); err != nil {
+		c.JSON(http.StatusBadRequest, result.NewFailedResult(err.Error()))
+		return
+	}
+	infoService := service.GetInfoService()
+	err2 := infoService().UpdateSchool(&school)
+	if err2 != nil {
+		c.JSON(http.StatusBadRequest, err2)
+	} else {
+		c.JSON(http.StatusOK, result.Ok())
+	}
 
 }
 
+// GetSchool
+//
+//	@Description: 根据id查询单个学校信息 参数:IdRequest
+//	@param c
 func GetSchool(c *gin.Context) {
-
+	var idRequest IdRequest
+	if err := c.ShouldBindUri(&idRequest); err != nil {
+		c.JSON(http.StatusBadRequest, result.NewFailedResult(err.Error()))
+		return
+	}
+	id, err := strconv.ParseInt(idRequest.Id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, result.NewFailedResult(err.Error()))
+		return
+	}
+	infoService := service.GetInfoService()
+	school, err2 := infoService().GetSchool(id)
+	if err2 != nil {
+		c.JSON(http.StatusBadRequest, err2)
+	} else {
+		c.JSON(http.StatusOK, result.NewOkResult(school))
+	}
 }
