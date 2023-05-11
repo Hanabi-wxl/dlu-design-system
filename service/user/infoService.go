42ad60c35f885af121e882260216942761ac4bf7
Tao <Forrest_Tao>
2023-05-11 21:45:33 +0800

    fix:修改interface中的函数声明返回值为指针

--- service/user/infoService.go
+++ service/user/infoService.go
@@ -6,8 +6,8 @@ import (
 
 type InfoService interface {
 	UserInfo()
-	GetUserById(id, isStu int) (User, *errno.Errno)
-	GetUserByNumber(number string, isStu int8) (User, *errno.Errno)
+	GetUserById(id int64, isStu int8) (*User, *errno.Errno)
+	GetUserByNumber(number string, isStu int8) (*User, *errno.Errno)
 	GetUsersByMajor(majorId, isStu, size, num int) (*[]User, *errno.Errno)
 	GetUsersByCollege(collegeId, isStu, size, num int) (*[]User, *errno.Errno)
 }
